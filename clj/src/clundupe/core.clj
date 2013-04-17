7d95dca19e9dae34bb7d59e8092f7cdd72e88350
James Uther <james@lshift.net>
2013-04-17 16:55:19 +0100

    changes

--- clj/src/clundupe/core.clj
+++ clj/src/clundupe/core.clj
@@ -1,15 +1,16 @@
 (ns clundupe.core
   (:use [clojure.java.io :as io]
         [clojure.pprint])
+  (:require [digest])
   (:gen-class))
 
-;((defmulti take-files ()) take-files [fseq])
+(defn walkf [root]
+  (filter #(.isFile %) (file-seq (io/file root))))
 
 (defn -main
   "I don't do a whole lot."
   [& args]
-  (for [f args
-    :let [fi (file-seq (io/file f))]]
-    (pprint fi)
-    ))
+  (pprint (pmap #(vector % (keyword (digest/md5 %))) (mapcat #(walkf %) args)))
+  (shutdown-agents)
+    )
 
